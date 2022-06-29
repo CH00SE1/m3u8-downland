14424d50d180b3a1489f4d6af626d8ae95fcd465
liushaoxiong <3354621952@qq.com>
2022-06-29 15:20:44 +0800

    添加数据连接

--- src/main/java/com/example/m3u8/controller/webController.java
+++ src/main/java/com/example/m3u8/controller/webController.java
@@ -5,9 +5,9 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.m3u8.entity.HsInfo;
 import com.example.m3u8.entity.HsInfoDTO;
 import com.example.m3u8.service.IHsInfoService;
+import lombok.extern.slf4j.Slf4j;
 import net.m3u8.utils.m3u8Utils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -18,11 +18,10 @@ import java.util.List;
  * @author Liushaoxiong
  * @date 2022-06-16 14:19:12
  */
+@Slf4j
 @RestController
-@RequestMapping(value = "/info")
+@RequestMapping(value = "/m3u8")
 public class webController {
-
-    private final Logger log = LoggerFactory.getLogger(webController.class);
 
     @Resource
     private IHsInfoService hsInfoService;
@@ -33,7 +32,7 @@ public class webController {
      * @param jsonObeject
      * @return
      */
-    @PostMapping(value = "/m3u8/download")
+    @PostMapping(value = "/download")
     public Object m3u8(@RequestBody JSONObject jsonObeject) {
         log.info("{}", jsonObeject);
         m3u8Utils m3u8Utils = new m3u8Utils();
@@ -47,14 +46,13 @@ public class webController {
      * @param title
      * @return
      */
-    @PostMapping(value = "/m3u8/list/{title}")
+    @PostMapping(value = "/list/{title}")
     public Object list(@PathVariable(name = "title") String title) {
         List<HsInfo> hsInfos = hsInfoService.list(new LambdaQueryWrapper<HsInfo>().like(HsInfo::getTitle, title));
         List<HsInfoDTO> hsInfoDTOs = new ArrayList<>();
         for (HsInfo hsInfo : hsInfos) {
             HsInfoDTO hsInfoDTO = new HsInfoDTO();
-            hsInfoDTO.setTitle(hsInfo.getTitle());
-            hsInfoDTO.setM3u8_url(hsInfo.getm3u8Url());
+            BeanUtils.copyProperties(hsInfo, hsInfoDTO);
             hsInfoDTOs.add(hsInfoDTO);
         }
         return hsInfoDTOs;
--- src/main/java/com/example/m3u8/entity/HsInfoDTO.java
+++ src/main/java/com/example/m3u8/entity/HsInfoDTO.java
@@ -1,36 +1,18 @@
 package com.example.m3u8.entity;
 
+import lombok.Data;
+
 /**
  * @author xiongshao
  * @date 2022-06-29 11:12:00
  */
+@Data
 public class HsInfoDTO {
 
     private String title;
 
-    private String m3u8_url;
+    private String url;
 
-    public String getTitle() {
-        return title;
-    }
+    private String m3u8Url;
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getM3u8_url() {
-        return m3u8_url;
-    }
-
-    public void setM3u8_url(String m3u8_url) {
-        this.m3u8_url = m3u8_url;
-    }
-
-    @Override
-    public String toString() {
-        return "HsInfoDTO{" +
-                "title='" + title + '\'' +
-                ", m3u8_url='" + m3u8_url + '\'' +
-                '}';
-    }
 }
